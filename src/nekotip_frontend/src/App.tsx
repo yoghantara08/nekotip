78d0334ae07ae36afe64845be660022fb48d8669
Purwa Wiadnyana <purwawiadnyana08@gmail.com>
2024-11-16 22:29:50 +0800

    commit setup frontend shadcn

--- src/nekotip_frontend/src/App.tsx
+++ src/nekotip_frontend/src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import { nekotip_backend } from "../../declarations/nekotip_backend";
+import { Button } from '@/components/ui/button'
+
+import { nekotip_backend } from '../../declarations/nekotip_backend'
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('')
 
   const handleGreet = async () => {
-    const result = await nekotip_backend.greet("NekoTip");
-    setText(result);
-  };
+    const result = await nekotip_backend.greet('NekoTip')
+    setText(result)
+  }
 
   return (
     <main className="text-3xl" onClick={handleGreet}>
-      Click me! <span className="text-red-400">{text}</span>
+      <Button type="button" variant="default">
+        Click me!
+      </Button>
+
+      <span className="ms-4 text-red-400">{text}</span>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/nekotip_frontend/vite.config.js
+++ src/nekotip_frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from 'url';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import environment from 'vite-plugin-environment';
-import dotenv from 'dotenv';
+import { fileURLToPath, URL } from 'url'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import environment from 'vite-plugin-environment'
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: '../../.env' })
 
 export default defineConfig({
   build: {
@@ -13,31 +14,29 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       define: {
-        global: "globalThis",
+        global: 'globalThis',
       },
     },
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:4943",
+      '/api': {
+        target: 'http://127.0.0.1:4943',
         changeOrigin: true,
       },
     },
   },
   plugins: [
     react(),
-    environment("all", { prefix: "CANISTER_" }),
-    environment("all", { prefix: "DFX_" }),
+    environment('all', { prefix: 'CANISTER_' }),
+    environment('all', { prefix: 'DFX_' }),
   ],
   resolve: {
-    alias: [
-      {
-        find: "declarations",
-        replacement: fileURLToPath(
-          new URL("../declarations", import.meta.url)
-        ),
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+
+      find: 'declarations',
+      replacement: fileURLToPath(new URL('../declarations', import.meta.url)),
+    },
   },
-});
+})
