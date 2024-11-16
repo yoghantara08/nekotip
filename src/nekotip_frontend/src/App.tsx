80175e83bac836e642904c5001f2d84a977d0ad3
yoghantara08 <gyoghantara@gmail.com>
2024-11-16 14:26:35 +0800

    hello world

--- src/nekotip_frontend/src/App.tsx
+++ src/nekotip_frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
+import { nekotip_backend } from "../../declarations/nekotip_backend";
+
 function App() {
-  return <main className="text-3xl text-red-400">Hello World!</main>;
+  const [text, setText] = useState("");
+
+  const handleGreet = async () => {
+    const result = await nekotip_backend.greet("NekoTip");
+    setText(result);
+  };
+
+  return (
+    <main className="text-3xl" onClick={handleGreet}>
+      Click me! <span className="text-red-400">{text}</span>
+    </main>
+  );
 }
 
 export default App;
