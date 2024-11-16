31b589a61a114fa2ac145c2abcd8449e42cab292
yoghantara08 <gyoghantara@gmail.com>
2024-11-16 13:16:02 +0800

    placeholder

---
+++ src/nekotip_frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  console.error("Root element not found");
+}
