4c55d94f89c1ff60469facdb3a5fef4f0ececb6b
yoghantara08 <gyoghantara@gmail.com>
2024-11-22 22:15:18 +0800

    bignumberjs

--- src/nekotip_frontend/src/components/features/Login/Login.tsx
+++ src/nekotip_frontend/src/components/features/Login/Login.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Principal } from '@dfinity/principal';
+import BigNumber from 'bignumber.js';
 
 import useAuth from '@/hooks/useAuth';
+import useICPLedgerPayment from '@/hooks/useICPLedgerPayment';
 import useUser from '@/hooks/useUser';
+
+import { canisterId } from '../../../../../declarations/nekotip_backend';
 
 const Login = () => {
   const { handleLogin, isAuthenticated, logoutUser, isLoading } = useAuth();
-  const { user } = useUser();
+  const { user, getICPBalance } = useUser();
+  const { transferICP } = useICPLedgerPayment();
+
+  const [balance, setBalance] = useState<bigint | undefined>(BigInt(0));
+  const [result, setResult] = useState<bigint>();
+
+  const transfer = async () => {
+    const recipientPrincipal = Principal.fromText(canisterId);
+    const e8sAmount = BigInt(
+      BigNumber(1)
+        .multipliedBy(10 ** 8)
+        .toNumber(),
+    );
+
+    const result = await transferICP(recipientPrincipal, e8sAmount);
+    if (result) setResult(result);
+  };
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const balance = await getICPBalance();
+      setBalance(balance);
+    };
+
+    fetchBalance();
+  }, []);
 
   return (
     <div>
@@ -16,6 +47,12 @@ const Login = () => {
           <div className="text-2xl font-bold">
             Wallet Address: {user?.depositAddress}
           </div>
+          <div className="text-2xl font-bold">
+            Balance: {balance?.toString()}
+          </div>
+          <div className="text-2xl font-bold">Result: {result?.toString()}</div>
+
+          <button onClick={transfer}>Transfer</button>
           <button onClick={logoutUser}>Logout</button>
         </>
       ) : (
