0d18c3a67902742fbf279795709583f605f8a0ea
yoghantara08 <gyoghantara@gmail.com>
2024-11-23 22:10:25 +0800

    adjust

--- src/nekotip_frontend/src/hooks/useUser.tsx
+++ src/nekotip_frontend/src/hooks/useUser.tsx
@@ -1,6 +1,8 @@
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { AuthClient } from '@dfinity/auth-client';
+
 import { serializeUser } from '@/lib/utils';
 import { AppDispatch, RootState } from '@/store';
 import { setReferralCode, setUser } from '@/store/reducers/userSlice';
@@ -16,7 +18,7 @@ const useUser = () => {
   const { user, referralCode } = useSelector((state: RootState) => state.user);
   const dispatch: AppDispatch = useDispatch();
 
-  const { getActor } = useActor();
+  const { getAuthenticatedActor } = useActor();
 
   const updateUser = (user: User | null) => {
     if (user) dispatch(setUser(serializeUser(user)));
@@ -25,7 +27,15 @@ const useUser = () => {
 
   const updateUserProfile = async (updateData: UserUpdateData) => {
     try {
-      const actor = await getActor();
+      const authClient = await AuthClient.create();
+      const isAuthenticated = await authClient.isAuthenticated();
+
+      if (!isAuthenticated) {
+        throw new Error('User not authenticated');
+      }
+
+      const identity = authClient.getIdentity();
+      const actor = await getAuthenticatedActor(identity);
       const result = await actor.updateUserProfile(updateData);
 
       if ('ok' in result) {
@@ -48,10 +58,18 @@ const useUser = () => {
 
   const getICPBalance = async () => {
     try {
-      const actor = await getActor();
+      const authClient = await AuthClient.create();
+      const isAuthenticated = await authClient.isAuthenticated();
+
+      if (!isAuthenticated) {
+        throw new Error('User not authenticated');
+      }
+
+      const identity = authClient.getIdentity();
+      const actor = await getAuthenticatedActor(identity);
       const result = await actor.getAccountBalance();
 
-      return result;
+      return result ?? 0;
     } catch (error) {
       console.error(error);
     }
