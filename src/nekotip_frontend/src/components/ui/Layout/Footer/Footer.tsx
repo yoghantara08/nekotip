a726d0f37eabd816813fa54494931dc04b7f1132
yoghantara08 <gyoghantara@gmail.com>
2024-11-24 16:44:57 +0800

    footer

--- src/nekotip_frontend/src/components/ui/Layout/Footer/Footer.tsx
+++ src/nekotip_frontend/src/components/ui/Layout/Footer/Footer.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const socials = [
+    { link: '#', icon: '/images/icons/instagram.svg' },
+    { link: '#', icon: '/images/icons/twitter.svg' },
+    { link: '#', icon: '/images/icons/facebook.svg' },
+  ];
+
+  return (
+    <footer className="flex h-[60px] w-full items-center justify-center border-t md:h-[75px]">
+      <div className="flex w-full max-w-[1280px] items-center justify-between px-4">
+        <div className="flex items-center gap-2">
+          {/* <p className="text-xl font-semibold text-subtext">©2024</p> */}
+          <img
+            src="/images/logo/nekotip-logo.svg"
+            alt="nekotip logo"
+            className="w-24 md:w-28"
+          />
+        </div>
+        <div className="flex items-center gap-8">
+          {socials.map((social, index) => (
+            <a
+              href={social.link}
+              key={index}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                key={index}
+                src={social.icon}
+                alt="social icon"
+                className="w-6 cursor-pointer hover:opacity-70 md:w-7"
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/nekotip_frontend/src/lib/utils/index.ts
+++ src/nekotip_frontend/src/lib/utils/index.ts
@@ -33,3 +33,18 @@ export const serializeUser = (user: User): ISerializedUser => {
     profilePic: user.profilePic[0] ?? null,
   };
 };
+
+export const openPage = (e: any, url: string) => {
+  if (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  // IOS issue when open new pop window
+  setTimeout(() => {
+    if (window.navigator.userAgent?.toLowerCase().includes('metamask')) {
+      window.location.assign(url);
+    } else {
+      window.open(url, '_blank');
+    }
+  });
+};
