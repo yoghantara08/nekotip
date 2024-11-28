e5cca6b3819238f9526056cb004914e4497e04e2
yoghantara08 <gyoghantara@gmail.com>
2024-11-28 13:02:16 +0800

    frontend: toggle follow user

--- src/nekotip_frontend/src/pages/user/ViewedProfilePage.tsx
+++ src/nekotip_frontend/src/pages/user/ViewedProfilePage.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-import { UserRoundPlusIcon } from 'lucide-react';
+import { Principal } from '@dfinity/principal';
+import { UserRoundCheckIcon, UserRoundPlusIcon } from 'lucide-react';
 
 import ExclusiveContentPanel from '@/components/features/ViewedProfile/ExclusiveContentPanel';
 import HomePanel from '@/components/features/ViewedProfile/HomePanel';
 import Layout from '@/components/ui/Layout/Layout';
 import useUser from '@/hooks/useUser';
 import { cn } from '@/lib/utils/cn';
+import { useAuthManager } from '@/store/AuthProvider';
 
 import { User } from '../../../../declarations/nekotip_backend/nekotip_backend.did';
 
@@ -15,32 +17,70 @@ const MENU_PROFILE = ['Home', 'Exclusive Contents'];
 
 const ViewedProfilePage = () => {
   const { username } = useParams();
-  const { getUserByUsername } = useUser();
+  const { getUserByUsername, user } = useUser();
+  const { actor, principal } = useAuthManager();
 
-  const [user, setUser] = useState<User | null>(null);
+  const [viewedUser, setViewedUser] = useState<User | null>(null);
+  const [notFound, setNotFound] = useState(false);
   const [menu, setMenu] = useState('Home');
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingToggleFollow, setLoadingToggleFollow] = useState(false);
 
   useEffect(() => {
-    if (username && !user) {
-      getUserByUsername(username).then((result) => {
-        if (result) setUser(result);
-        console.log('test');
-      });
+    if (username && !viewedUser) {
+      setIsLoading(true);
+      getUserByUsername(username)
+        .then((result) => {
+          if (result) {
+            setViewedUser(result);
+            setNotFound(false);
+          } else {
+            setNotFound(true);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        })
+        .finally(() => setIsLoading(false));
     }
-  }, [getUserByUsername, user, username]);
+  }, [getUserByUsername, viewedUser, username]);
 
-  if (user)
+  const toggleFollow = async (target: Principal) => {
+    try {
+      setLoadingToggleFollow(true);
+      if (viewedUser && actor) {
+        const result = await actor.toggleFollow(target);
+
+        if ('ok' in result) {
+          const updatedUser = await getUserByUsername(username!);
+          if (updatedUser) {
+            setViewedUser(updatedUser);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error following user:', error);
+    } finally {
+      setLoadingToggleFollow(false);
+    }
+  };
+
+  const isFollowing = viewedUser?.followers.some(
+    (follower) => follower.toText() === principal?.toText(),
+  );
+
+  if (viewedUser && !isLoading && !notFound)
     return (
-      <Layout title={user.name[0]}>
+      <Layout title={viewedUser.name[0]}>
         <div className="w-full rounded-lg border text-subtext shadow-custom">
           <div className="relative">
             <img
-              src={user?.bannerPic[0] || '/images/default-banner.png'}
+              src={viewedUser?.bannerPic[0] || '/images/default-banner.png'}
               alt="banner"
               className="h-[200px] w-full rounded-t-lg object-cover"
             />
             <img
-              src={user?.profilePic[0] || '/images/user-default.svg'}
+              src={viewedUser?.profilePic[0] || '/images/user-default.svg'}
               alt="profile"
               className="absolute -bottom-10 left-5 size-20 rounded-full md:size-28"
             />
@@ -49,28 +89,44 @@ const ViewedProfilePage = () => {
             <section className="flex items-center justify-between gap-3">
               <div>
                 <h1 className="text-2xl font-semibold text-title">
-                  {user.name[0]}
+                  {viewedUser.name[0]}
                 </h1>
-                <p className="text-lg text-subtext">@{user.username}</p>
+                <p className="text-lg text-subtext">@{viewedUser.username}</p>
               </div>
-              <div>
-                <button className="flex items-center gap-2 rounded-lg border px-4 py-2 font-semibold text-subtext hover:bg-mainAccent">
-                  <UserRoundPlusIcon />
-                  Follow
+
+              {user && user.username !== viewedUser.username && (
+                <button
+                  disabled={loadingToggleFollow}
+                  className={cn(
+                    'flex items-center gap-2 rounded-lg border px-4 py-2 font-semibold text-subtext hover:bg-thirdAccent/50',
+                    isFollowing && 'bg-thirdAccent',
+                    loadingToggleFollow &&
+                      'cursor-not-allowed bg-thirdAccent/50',
+                  )}
+                  onClick={() => toggleFollow(viewedUser.id)}
+                >
+                  {isFollowing ? <UserRoundCheckIcon /> : <UserRoundPlusIcon />}
+                  {isFollowing
+                    ? 'Followed'
+                    : loadingToggleFollow
+                      ? 'Following...'
+                      : 'Follow'}
                 </button>
-              </div>
+              )}
             </section>
 
             <section className="mt-2 space-y-5 md:mt-4">
               <div className="md:hidden">
                 <div className="text-caption">
-                  <span>{user.bio[0] || 'This user has no bio yet.'}</span>
+                  <span>
+                    {viewedUser.bio[0] || 'This user has no bio yet.'}
+                  </span>
                 </div>
                 <Link
-                  to={`/explore/${user.categories.toLocaleString().toLowerCase()}`}
+                  to={`/explore/${viewedUser.categories.toLocaleString().toLowerCase()}`}
                   className="mt-2 block w-fit rounded-lg border px-4 py-2 text-sm font-medium hover:bg-mainAccent/30"
                 >
-                  {user.categories}
+                  {viewedUser.categories}
                 </Link>
               </div>
               <div className="flex w-fit items-center gap-4">
@@ -87,13 +143,22 @@ const ViewedProfilePage = () => {
                   </button>
                 ))}
               </div>
-              {menu === 'Home' && <HomePanel user={user} />}
+              {menu === 'Home' && <HomePanel user={viewedUser} />}
               {menu === 'Exclusive Contents' && (
-                <ExclusiveContentPanel creatorId={user.id} />
+                <ExclusiveContentPanel creatorId={viewedUser.id} />
               )}
             </section>
           </main>
         </div>
+      </Layout>
+    );
+
+  if (notFound && !isLoading)
+    return (
+      <Layout>
+        <p className="font-montserrat text-xl font-medium text-subtext">
+          404 User not found...
+        </p>
       </Layout>
     );
 
